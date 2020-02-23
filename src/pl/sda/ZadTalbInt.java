55f3b802aaa20c5ad5e9cbf81c2bf2e35008ea5d
Maciej Czulak <maciejczulak@o2.pl>
2020-02-23 10:22:56 +0100

    Find max and min value of table

--- src/pl/sda/ZadTalbInt.java
+++ src/pl/sda/ZadTalbInt.java
@@ -1,7 +1,31 @@
 package pl.sda;
 
 public class ZadTalbInt {
+    int table[];
+    public static int findMax(int[] table) {
+        int max = table[0];
+        for (int i = 1; i < table.length; i++) {
+            if (table[i] > max) {max=table[i];}}
+            return max; }
+
+    public static int findMin(int[] table) {
+        int min = table[0];
+        for (int i = 1; i < table.length; i++) {
+            if (table[i] < min) {min=table[i];}}
+        return min; }
+
     public static void main(String[] args) {
-        
-    }
-}
+        int[] tableInt = {4, 67, 1, 21, 3, 8, 47, 92, 5};
+        System.out.println(findMax(tableInt));
+        System.out.println(findMin(tableInt));
+    }}
+
+
+
+
+
+
+
+
+
+
